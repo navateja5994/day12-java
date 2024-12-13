913576df21fd03f8f6539184591230ead3af4eef
NAVATEJA <navatejar80@gmail.com>
2024-12-13 14:56:24 +0530

    Add files via upload

---
+++ foreach.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) {
+        HashMap<String, String> a = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            String s1 = sc.next();
+            a.put(s, s1);
+        }
+        
+        for (String s : a.keySet()) {
+            System.out.println(s + ":" + a.get(s));
+        }
+        
+        sc.close();
+    }
+}
