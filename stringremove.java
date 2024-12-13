44d4e715468876d2e50f9d5d27ee0d4fbb5b88d6
NAVATEJA <navatejar80@gmail.com>
2024-12-13 12:28:50 +0530

    Add files via upload

---
+++ stringremove.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        String arr1[]={"nava","teja","ds"};
+        String arr2[]={"ds","2 year","btech"};
+        Set <String> a=new HashSet<>();
+        Set <String> b=new HashSet<>();
+        Set <String> c=new HashSet<>();
+        for(String s:arr1)
+        a.add(s);
+        for(String s1:arr2)
+        b.add(s1);
+        System.out.println(a);
+        System.out.println(b);
+        Set <String> u=new HashSet<>(a);
+        u.retainAll(b);
+        System.out.println(u);
+    }
+}
